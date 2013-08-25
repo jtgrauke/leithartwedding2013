6709678759e560a48498b5934e10dc8d88cab9a7
JT Grauke <jtgrauke@gmail.com>
2013-08-24 18:16:24 -0700

    Fixed show more function

--- application.js
+++ application.js
@@ -7,36 +7,59 @@ Instagram.Config = {
     apiHost: 'https://api.instagram.com'
 };
 
-var config = Instagram.Config, url;
+var config = Instagram.Config;
+var max = '';
 
-function loadInstagrams(photos) {
 
-    var tag = 'leithartwedding2013';
-    var max = '';
+
+
+function generateURL(){
+  var tag = 'leithartwedding2013';
+  url = config.apiHost + "/v1/tags/" + tag + "/media/recent?callback=?&client_id=" + config.clientID;
+
+  return function(max_id) {
+
+    if(typeof max_id === 'string' && max_id.trim() !== '') {
+      url += "&max_tag_id=" + max_id;
+    }
+    return url;
+  };
+
+} generateURL();
+
+
+
+function loadInstagrams(max_id) {
 
     $.ajax({
-            type: "GET",
-            url: config.apiHost + "/v1/tags/" + tag + "/media/recent?callback=?&client_id=" + config.clientID + max,
-            dataType: "jsonp"
-        }).done(function(photos){
-            console.log(photos);
-            for(i=0;i<photos.data.length;i++){
-                var img = photos.data[i].images.low_resolution.url;
-                var link = photos.data[i].link;
-                $('ul').append("<li><a href='"+link+"'><img src='"+img+"'></a></li>");
-                $('.paginate a').attr('data-max-tag-id', photos.pagination.next_max_id).fadeIn();
-            }
-        });
+        type: "GET",
+        url: url,
+        data: {'client_id': config.clientID, 'max_tag_id': max},
+        dataType: "jsonp",
+        beforeSend: function(){$('.feed li a img').fadeIn();},
+        complete: function(){$('.feed li a img').fadeIn(1000);}
+    }).done(function(data){
+        console.log(data);
+        for(i=0;i<data.data.length;i++){
+            var img = data.data[i].images.standard_resolution.url;
+            var link = data.data[i].link;
+            var photo = "<li><a target='_blank' href='"+link+"'><img src='"+img+"'></a></li>";
+            $('ul').append(photo);
+            $('.paginate a').attr('data-max-tag-id', data.pagination.next_max_id).fadeIn();
+        }
 
-}
-
-function showMore(){
-    $('body').on('click', '.paginate a.view-more', function(){
-        loadInstagrams();
-        return false;
+        max = data.pagination.next_max_tag_id;
     });
 }
 
+
+function showMore(){
+
+    $('body').on('click', '.paginate a.view-more', function(e) {
+        e.preventDefault();
+        loadInstagrams();
+    });
+}
 
 
 $(document).ready(function(){
